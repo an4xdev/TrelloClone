8fdbdaf1820282410d3dddc59924b6fa0003f9ab
Michał Żuk <michal.zuk30601@gmail.com>
2024-08-01 14:34:55 +0200

    boilerplate for edit template modal, changed add template modal

---
+++ Trello.Shared/Requests/ChangeTemplateRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Trello.Shared.Requests;
+
+public class ChangeTemplateRequest
+{
+    public int ID { get; set; }
+
+    public string Name { get; set; }
+
+    public List<AddTemplateColumn> AddedColumns { get; set; }
+
+    public List<ChangedColumn> ChangedColumns { get; set; }
+
+    public List<int> DeletedColumnIDs { get; set; }
+
+}
+
+public record ChangedColumn(int ID, string Name);
